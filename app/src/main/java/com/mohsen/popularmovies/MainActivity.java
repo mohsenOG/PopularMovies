aa2e3dd555686479a9e175722afbddd5fd18f8a5
Mohsen OG <mohsen_og@yahoo.com>
2018-02-20 10:10:58 +0100

    adding settings activity.

--- app/src/main/java/com/mohsen/popularmovies/MainActivity.java
+++ app/src/main/java/com/mohsen/popularmovies/MainActivity.java
@@ -1,13 +1,68 @@
 package com.mohsen.popularmovies;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the preferences to default for each cold start of app.
+        PreferenceManager.setDefaultValues(this, R.xml.pref_popular_movies, true);
+
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_setting) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        //TODO if changed re arrange the movies.
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
     }
 }
---
+++ app/src/main/java/com/mohsen/popularmovies/settingsFragment.java
@@ -0,0 +1,16 @@
+package com.mohsen.popularmovies;
+
+/**
+ * Created by Mohsen on 19.02.2018.
+ */
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+
+public class settingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_popular_movies);
+    }
+}
